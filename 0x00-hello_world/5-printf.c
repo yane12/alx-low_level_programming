9ec81dd8668847b6d1a671cf73c0ee58f171c153
yane12 <yane12girma@gmail.com>
2022-12-08 08:30:46 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: 0 
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
  */
 int main(void)
 {
