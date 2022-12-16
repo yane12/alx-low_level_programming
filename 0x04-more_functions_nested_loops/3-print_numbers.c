deda882f2902ed223466d8f1d05e245bc68b3b34
yane12 <yane12girma@gmail.com>
2022-12-16 05:08:47 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_numbers(void)
 {
-  int i;
+int i;
 
-  for (i = 48; i <= 57; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
