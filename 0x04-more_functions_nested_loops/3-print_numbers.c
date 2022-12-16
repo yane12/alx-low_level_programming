f8298f1cd23c52383650e648a5a43b967865e620
yane12 <yane12girma@gmail.com>
2022-12-16 04:31:55 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - display all number from 0 to 9 followed by new line.
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+  int i;
+
+  for (i = 48; i <= 57; i++)
+    {
+      _putchar(i);
+    }
+  _putchar('\n');
+}
