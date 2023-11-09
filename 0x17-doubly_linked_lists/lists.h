059504c4ec30fc81919cfd00e5c2691f4b7eca26
yane12 <yane12girma@gmail.com>
2023-11-09 02:13:22 -0800

    lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,9 +9,6 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * for Holberton project
  */
 typedef struct dlistint_s
 {
